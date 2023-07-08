0ba0aafde538ef5a60e69e7c1f062371d411f4e2
JA Simmons V <simmonsj@redkeep.com>
2023-07-08 16:56:08 -0500

    Restructure project folders

---
+++ src/warrenBot/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""Top-level package for warrenbot
+
+warren_bot
+Copyright (c) 2023 J.A. Simmons V
+Full license in LICENSE.md
+"""
+
+__author__ = "J.A. Simmons V"
+__email__ = "simmonsj@jasimmonsv.com"
+__version__ = "0.1.1"
