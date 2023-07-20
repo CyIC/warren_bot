d6a57db001b6970b2cb31cc759306892e642633d
jasimmonsv <simmonsj@jasimmonsv.com>
2023-07-19 23:09:39 -0500

    Initial research into SlackBots.

--- slackchat.py
+++ slackchat.py
@@ -17,16 +17,69 @@ logger = logging.getLogger('slack')
 
 import os
 # Use the package we installed
-from slack_bolt import App
+from slack_bolt import App, Say
 
 # Initializes your app with your bot token and signing secret
 app = App(
-    token=os.environ.get("SLACK_BOT_TOKEN"),
-    signing_secret=os.environ.get("SLACK_SIGNING_SECRET")
+    token=SLACK_BOT_TOKEN,
+    signing_secret=SLACK_SIGNING_SECRET
 )
 
 # Add functionality here
-# @app.event("app_home_opened") etc
+@app.event("app_home_opened")
+def update_home_tab(client, event, logger):
+    try:
+        # views.publish is the method that your app uses to push a view to the Home tab
+        client.views_publish(
+            # the user that opened your app's app home
+            user_id=event["user"],
+            # the view object that appears in the app home
+            view={
+                "type": "home",
+                "callback_id": "home_view",
+                # body of the view
+                "blocks": [
+                    {
+                        "type": "section",
+                        "text": {
+                            "type": "mrkdwn",
+                            "text": "*Welcome to your _App's Home_* :tada:"
+                        }
+                    },
+                    {
+                        "type": "divider",
+                    },
+                    {   "type": "section",
+                        "text": {
+                            "type": "mrkdwn",
+                            "text": "This button won't do much for now but you can setup a listener for it using the `actions()` method and passing its unique `action_id`"
+                        }
+                    },
+                    {   "type": "actions",
+                        "elements": [
+                            {
+                                "type": "button",
+                                "text": {
+                                    "type": "plain_text",
+                                    "text": "Click Me!"
+                                }
+                            }
+                        ]
+                    }
+                ]
+            }
+        )
+    except Exception as e:
+        logger.error(f"Error publishing home tab: {e}")
+
+
+@app.message("test")
+def reply_to_test(say):
+    say("Yes, tests are important!")
+
+@message.im("test")
+def reply_to_test(say):
+    say("yes, test away")
 
 
 # Start your app
